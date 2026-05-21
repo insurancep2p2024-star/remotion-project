a0a144c4032309443d1c16c9895ddd46ebb4f937
insurancep2p2024-star <insurancep2p2024@gmail.com>
2026-05-21 10:01:52 -0400

    fix: use remotion package instead of @remotion/core

--- src/scenes/Scene1Hook.tsx
+++ src/scenes/Scene1Hook.tsx
@@ -1,5 +1,5 @@
 import { FONT_SANS, FONT_MONO } from "../fonts";
-import { AbsoluteFill, useCurrentFrame, useVideoConfig, interpolate, spring } from "@remotion/core";
+import { AbsoluteFill, useCurrentFrame, useVideoConfig, interpolate, spring } from "remotion";
 
 export const Scene1Hook = () => {
   const frame = useCurrentFrame();
